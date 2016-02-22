29e95a12a309018e54ff15551af3604ce330eefb
P Karthikeyan <karthi.email@gmail.com>
2016-02-23 10:56:55 +1100

    removed failing unit tests to test ci

--- WebService.UnitTests/SampleTest.cs
+++ WebService.UnitTests/SampleTest.cs
@@ -15,12 +15,6 @@ namespace WebService.UnitTests
             Assert.Equal(4, Add(2, 2));
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
-
         int Add(int x, int y)
         {
             return x + y;
