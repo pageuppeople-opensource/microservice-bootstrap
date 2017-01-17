75651709b34d12a9f5c308ec8d149d00afeda583
Jim Pelletier <mail@thejimmyp.com>
2017-01-18 10:59:57 +1100

    Added Serilog logger

--- WebService.UnitTests/ValueControllerTest.cs
+++ WebService.UnitTests/ValueControllerTest.cs
@@ -1,15 +1,15 @@
-﻿//using Xunit;
-//using WebService.Controllers;
-//
-//namespace WebService.UnitTests
-//{
-//    public class ValueControllerTest
-//    {
-//        [Fact]
-//        public void DoesNotBlowUpOnLogger()
-//        {
-//            var controller = new ValuesController();
-//            var result = controller.Get();
-//        }
-//    }
-//}
+﻿using Xunit;
+using WebService.Controllers;
+
+namespace WebService.UnitTests
+{
+    public class ValueControllerTest
+    {
+        [Fact]
+        public void DoesNotBlowUpOnLogger()
+        {
+            var controller = new ValuesController();
+            var result = controller.Get();
+        }
+    }
+}
